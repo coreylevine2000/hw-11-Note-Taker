ff99b2600362e66508100b76d20971818a19cbd2
Corey Levine <coreylevine2000@gmail.com>
2022-05-20 22:39:25 -0400

    are you fucking kidding me! it was one dot that caused so much hell in the server file!

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -9,7 +9,7 @@ router.get('/notes', (req, res) => {
 
 //all other routes respond with the index.html file
 router.get('*',(req, res) => {
-    res.sendFile(path.join(__dirname))
+    res.sendFile(path.join(__dirname, '../public/index.html'));
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -1,13 +1,13 @@
 const express = require('express');
 const apiRoutes = require('./routes/apiRoutes');
-const htmlRoutes = require('./routes./htmlRoutes');
+const htmlRoutes = require('./routes/htmlRoutes');
 
 //initialize app and create a port
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 
