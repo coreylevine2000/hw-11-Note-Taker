111d48532111feba159d51e9b913f3dc7afe1499
Corey Levine <coreylevine2000@gmail.com>
2022-05-20 23:15:12 -0400

    following along with lesson part 3

--- db/store.js
+++ db/store.js
@@ -1,2 +1,39 @@
 const fs = require('fs');
-const uuidv1 = require('uuid')
+
+//to generate unique ids
+const uuidv1 = require('uuid/v1');
+const util = require('util');
+
+const readFileAsync = util.promisify(fs.readFile);
+const writeFileAsync = util.promisify(fs.writeFile);
+
+class Store {
+    getNotes() {
+        return readFileAsync('db/db.json', 'utf8').then((notes) => {
+            return JSON.parse(notes)
+            // try {
+            //     parsedNotes = [].concat(JSON.parse(notes))
+            // } catch (err) {
+            //     parsedNotes = []
+            // }
+
+        })
+    }
+
+    addNote(note) {
+        const { title, text } = note;
+
+        if (!title|| !text) {
+            throw new Error('Title and text are required')
+        }
+
+    const newNote = {title, text, id: uuidv1() };
+    console.log(this.getNotes());
+
+    // this.getNotes().then(() => {
+
+    // })
+    }
+}
+
+module.exports = new Store();
